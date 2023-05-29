b59efae16f6bb1e98142a1b7d9ce3f04e903e960
sudeepsawant10 <sudeepsawant3710@gmail.com>
2023-05-29 16:35:08 +0530

    some changes

--- 10_callback_hell.js
+++ 10_callback_hell.js
@@ -3,6 +3,7 @@ const cart = ["shoes", "pants", "kurta"];
 // pyramid of doom = many levels of nested indentation
 // Invertion control = code gets out of control in a loop
 // asynch exist because of callback exist
+// cb are syncronous in nature 
 /**
  api.createOrder(cart function(){
     api.proceedToPayment(function(){
